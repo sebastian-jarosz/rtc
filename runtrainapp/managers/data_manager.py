8b8b5d1fff2477f740f9b4d4997ebb6f6942808e
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2022-01-27 19:32:36 +0100

    create and retrieve methods added

--- runtrainapp/managers/data_manager.py
+++ runtrainapp/managers/data_manager.py
@@ -1,4 +1,6 @@
 from runtrainapp.models import *
+from ..utils.constants import *
+from ..utils.form_constants import *
 
 
 def get_training_provider_by_name(provider_name):
@@ -9,8 +11,12 @@ def get_training_type_by_id(type_id):
     return TrainingType.objects.get(id=type_id)
 
 
+# Return training type by description or Type Other
 def get_training_type_by_description(description):
-    return TrainingType.objects.get(description=description)
+    try:
+        return TrainingType.objects.get(description=description)
+    except TrainingType.DoesNotExist:
+        return TrainingType.objects.get(id=TYPE_OTHER)
 
 
 def get_running_training_type_by_id(type_id):
@@ -90,5 +96,96 @@ def create_running_training(training, distance, avg_speed, running_training_type
         print("Running Training created\t- Training ID: %i\t Distance: %f\t Avg Speed: %f" % (training.id, distance, avg_speed))
     else:
         print("Running Training exists\t- Training ID: %i\t Distance: %f\t Avg Speed: %f" % (training.id, distance, avg_speed))
+
+    return obj
+
+
+# General method to create FormResponse and related ResponseOtherTraining and ResponseWellness
+# data_dict is used to retrieve all parsed data
+def create_form_response_and_related_data_by_dict(data_dict):
+    form_response_obj = create_form_response_by_dict(data_dict)
+
+    other_trainings_list = data_dict[INDEX_OTHER_TRAININGS_TYPES]
+    create_response_other_trainings_from_list(other_trainings_list, form_response_obj)
+
+    wellness_list = data_dict[INDEX_WELLNESS_TYPES]
+    create_response_wellness_from_list(wellness_list, form_response_obj)
+
+
+# Create ResponseWellness object and save into DB or return existing object
+def create_form_response_by_dict(data_dict):
+    obj, created = FormResponse.objects.get_or_create(
+        year_of_birth=data_dict[INDEX_YEAR_OF_BIRTH],
+        height=data_dict[INDEX_HEIGHT],
+        weight=data_dict[INDEX_WEIGHT],
+        running_years=data_dict[INDEX_RUNNING_YEARS],
+        time_1km=data_dict[INDEX_TIME_1KM],
+        time_5km=data_dict[INDEX_TIME_5KM],
+        time_10km=data_dict[INDEX_TIME_10KM],
+        time_21km=data_dict[INDEX_TIME_21KM],
+        time_42km=data_dict[INDEX_TIME_42KM],
+        training_amount=data_dict[INDEX_TRAINING_AMOUNT],
+        km_amount=data_dict[INDEX_KM_AMOUNT],
+        speed_training_amount=data_dict[INDEX_SPEED_TRAINING_AMOUNT],
+        minute_per_km_speed_training=data_dict[INDEX_MINUTE_PER_KM_SPEED_TRAINING],
+        threshold_training_amount=data_dict[INDEX_THRESHOLD_TRAINING_AMOUNT],
+        minute_per_km_threshold_training=data_dict[INDEX_MINUTE_PER_KM_THRESHOLD_TRAINING],
+        interval_training_amount=data_dict[INDEX_INTERVAL_TRAINING_AMOUNT],
+        minute_per_km_interval_training=data_dict[INDEX_MINUTE_PER_KM_INTERVAL_TRAINING],
+        run_up_training_amount=data_dict[INDEX_RUN_UP_TRAINING_AMOUNT],
+        minute_per_km_run_up_training=data_dict[INDEX_MINUTE_PER_KM_RUN_UP_TRAINING],
+        runway_amount=data_dict[INDEX_RUNWAY_AMOUNT],
+        km_per_runway=data_dict[INDEX_KM_PER_RUNWAY],
+        minute_per_km_runway=data_dict[INDEX_MINUTE_PER_KM_RUNWAY],
+        other_trainings=data_dict[INDEX_OTHER_TRAININGS],
+        other_trainings_amount=data_dict[INDEX_OTHER_TRAININGS_AMOUNT],
+        other_trainings_time=data_dict[INDEX_OTHER_TRAININGS_TIME],
+        wellness=data_dict[INDEX_WELLNESS],
+        wellness_amount=data_dict[INDEX_WELLNESS_AMOUNT],
+        detraining_amount=data_dict[INDEX_DETRAINING_AMOUNT],
+        detraining_days=data_dict[INDEX_DETRAINING_DAYS],
+        warmup=data_dict[INDEX_WARMUP],
+        warmup_time=data_dict[INDEX_WARMUP_TIME]
+    )
+
+    return obj
+
+
+# Create ResponseOtherTraining objects related to FormResponse
+def create_response_other_trainings_from_list(other_trainings_list, related_form_response):
+    created_objects_list = []
+    for other_training in other_trainings_list:
+        obj = create_response_other_training(related_form_response, other_training)
+        created_objects_list.append(obj)
+
+    return created_objects_list
+
+
+# Create ResponseOtherTraining object and save into DB or return existing object
+def create_response_other_training(form_response, other_training):
+    obj, created = ResponseOtherTraining.objects.get_or_create(
+        response=form_response,
+        other_training_type=other_training
+    )
+
+    return obj
+
+
+# Create ResponseWellness objects related to FormResponse
+def create_response_wellness_from_list(wellness_type_list, related_form_response):
+    created_objects_list = []
+    for wellness_type in wellness_type_list:
+        obj = create_response_wellness(related_form_response, wellness_type)
+        created_objects_list.append(obj)
+
+    return created_objects_list
+
+
+# Create ResponseWellness object and save into DB or return existing object
+def create_response_wellness(form_response, wellness_type):
+    obj, created = ResponseWellness.objects.get_or_create(
+        response=form_response,
+        wellness_type=wellness_type
+    )
 
     return obj
