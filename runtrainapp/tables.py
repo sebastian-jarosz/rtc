9ed61bcca38ef139f5583744700a32735aa1d923
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2022-02-17 00:10:53 +0100

    tables.py translated

--- runtrainapp/tables.py
+++ runtrainapp/tables.py
@@ -5,8 +5,12 @@ from .models import *
 
 
 class TrainingTable(tables.Table):
-    time = tables.TemplateColumn('{{ record.get_formatted_time }}')
-    start_date = tables.TemplateColumn('{{ record.get_formatted_start_date }}')
+    user = tables.Column(verbose_name="Użytkownik")
+    type = tables.Column(verbose_name="Typ")
+    start_date = tables.TemplateColumn('{{ record.get_formatted_start_date }}', verbose_name="Czas rozpoczęcia")
+    time = tables.TemplateColumn('{{ record.get_formatted_time }}', verbose_name="Długość treningu")
+    provider = tables.Column(verbose_name="Sposób dodania")
+    external_code = tables.Column(verbose_name="Zewnętrzne ID")
 
     class Meta:
         model = Training
@@ -14,16 +18,20 @@ class TrainingTable(tables.Table):
 
 
 class RunningTrainingTable(tables.Table):
+    training = tables.LinkColumn('training', args=[A('training.id')], verbose_name="Trening")
+    type = tables.Column(verbose_name="Typ")
+
     # Columns from Training Model
-    user = tables.TemplateColumn('{{ record.training.user }}')
-    start_date = tables.TemplateColumn('{{ record.training.get_formatted_start_date }}')
-    time = tables.TemplateColumn('{{ record.training.get_formatted_time }}')
-    provider = tables.TemplateColumn('{{ record.training.provider }}')
-    external_code = tables.TemplateColumn('{{ record.training.external_code }}')
+    user = tables.TemplateColumn('{{ record.training.user }}', verbose_name="Użytkownik")
+    start_date = tables.TemplateColumn('{{ record.training.get_formatted_start_date }}', verbose_name="Czas rozpoczęcia")
+    time = tables.TemplateColumn('{{ record.training.get_formatted_time }}', verbose_name="Długość treningu")
+    provider = tables.TemplateColumn('{{ record.training.provider }}', verbose_name="Sposób dodania")
+    external_code = tables.TemplateColumn('{{ record.training.external_code }}', verbose_name="Zewnętrzne ID")
 
     # Columns from RunningTraining model
-    distance = tables.TemplateColumn('{{ record.get_distance_km }}', verbose_name='Distance in km')
-    avg_speed = tables.TemplateColumn('{{ record.get_avg_speed_km_per_h }}', verbose_name='Avg Speed in km/h')
+    distance = tables.TemplateColumn('{{ record.get_distance_km }}', verbose_name='Dystans w km')
+    avg_speed = tables.TemplateColumn('{{ record.get_avg_speed_km_per_h }}', verbose_name='Prędkość w km/h')
+    segments_amount = tables.TemplateColumn('{{ record.segments_amount }}', verbose_name='Ilość segmentów')
 
     class Meta:
         model = RunningTraining
@@ -32,7 +40,7 @@ class RunningTrainingTable(tables.Table):
 
 class FormResponsesTable(tables.Table):
     response = tables.TemplateColumn('{{ record }}', order_by="id")
-    show_details = tables.LinkColumn('response', args=[A('id')], order_by="id", text='Show details')
+    show_details = tables.LinkColumn('response', args=[A('id')], order_by="id", text='Więcej informacji')
 
     class Meta:
         sequence = ('response', 'show_details')
