2e6d7eac2ab8be2188ef45a6928314c58eb212b9
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2022-02-16 20:59:01 +0100

    generator_result_manager.py working

--- runtrainapp/managers/generator_result_manager.py
+++ runtrainapp/managers/generator_result_manager.py
@@ -1,5 +1,6 @@
 from runtrainapp.models import *
 from runtrainapp.utils.generator_constants import *
+from runtrainapp.managers.data_manager import *
 import pandas as pd
 
 
@@ -32,11 +33,23 @@ def get_generator_input_df(main_form_dict, first_improved_form_dict, second_impr
     first_improved_gen_input_values = []
     second_improved_gen_input_values = []
 
-    # key question, value question id
-    for key, value in Q_GENERATOR_INPUT.items():
-        main_form_gen_input_values.append(main_form_dict[key])
-        first_improved_gen_input_values.append(first_improved_form_dict[key])
-        second_improved_gen_input_values.append(second_improved_form_dict[key])
+    # key - column, value - question
+    for column, question in Q_GENERATOR_INPUT.items():
+        # if column is marked as a training reference one, get reference value description
+        if column in GENERATOR_INPUT_TRN_REF_COLUMNS:
+            main_form_gen_input_values.append(get_training_timing_description_by_id(main_form_dict[column]))
+            first_improved_gen_input_values.append(get_training_timing_description_by_id(first_improved_form_dict[column]))
+            second_improved_gen_input_values.append(get_training_timing_description_by_id(second_improved_form_dict[column]))
+        # if column is warmup frequency ref
+        elif column == COL_WARMUP:
+            main_form_gen_input_values.append(get_warmup_freq_description_by_id(main_form_dict[column]))
+            first_improved_gen_input_values.append(get_warmup_freq_description_by_id(first_improved_form_dict[column]))
+            second_improved_gen_input_values.append(get_warmup_freq_description_by_id(second_improved_form_dict[column]))
+        # if column doesn't contain reference value - pass value itself
+        else:
+            main_form_gen_input_values.append(main_form_dict[column])
+            first_improved_gen_input_values.append(first_improved_form_dict[column])
+            second_improved_gen_input_values.append(second_improved_form_dict[column])
 
     input_df_data = {
         "": Q_GENERATOR_INPUT.values(),
@@ -59,11 +72,14 @@ def get_generator_output_df(main_form_dict, first_improved_form_dict, second_imp
     first_improved_gen_output_values = []
     second_improved_gen_output_values = []
 
-    # key question, value question id
-    for key, value in Q_GENERATOR_OUTPUT.items():
-        main_form_gen_output_values.append(main_form_dict[key])
-        first_improved_gen_output_values.append(first_improved_form_dict[key])
-        second_improved_gen_output_values.append(second_improved_form_dict[key])
+    # key - column, value - question
+    for column, question in Q_GENERATOR_OUTPUT.items():
+        main_dsc = get_distance_timing_desc_by_col_name_and_id(column, main_form_dict[column])
+        main_form_gen_output_values.append(main_dsc)
+        first_dsc = get_distance_timing_desc_by_col_name_and_id(column, first_improved_form_dict[column])
+        first_improved_gen_output_values.append(first_dsc)
+        second_dsc = get_distance_timing_desc_by_col_name_and_id(column, second_improved_form_dict[column])
+        second_improved_gen_output_values.append(second_dsc)
 
     output_df_data = {
         "": Q_GENERATOR_OUTPUT.values(),
@@ -75,3 +91,22 @@ def get_generator_output_df(main_form_dict, first_improved_form_dict, second_imp
     output_df = pd.DataFrame(output_df_data)
 
     return output_df
+
+
+# Get specific distance timing basing on column name and ref id
+# column_name - value used to match proper model
+# id - reference id
+def get_distance_timing_desc_by_col_name_and_id(col_name, timing_id):
+    if col_name == COL_TIME_1KM:
+        description = get_timing_by_model_and_id(OneKmTiming, timing_id).description
+    elif col_name == COL_TIME_5KM:
+        description = get_timing_by_model_and_id(FiveKmTiming, timing_id).description
+    elif col_name == COL_TIME_10KM:
+        description = get_timing_by_model_and_id(TenKmTiming, timing_id).description
+    elif col_name == COL_TIME_21KM:
+        description = get_timing_by_model_and_id(HalfMarathonTiming, timing_id).description
+    elif col_name == COL_TIME_42KM:
+        description = get_timing_by_model_and_id(MarathonTiming, timing_id).description
+
+    return description
+
