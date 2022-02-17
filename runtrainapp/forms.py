56f044e08ca38734e6fd57a88ced66f3fdca6ebf
Sebastian Jarosz <sebastian_jarosz@o2.pl>
2022-02-17 19:37:30 +0100

    GenerateTrainingForm change hardcoded question to constants

--- runtrainapp/forms.py
+++ runtrainapp/forms.py
@@ -5,6 +5,8 @@ from django import forms
 from runtrainapp.managers.data_manager import *
 from datetime import datetime
 
+from runtrainapp.utils.generator_constants import *
+
 
 def get_current_date_time():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -54,6 +56,26 @@ class ShowTrainingForm(forms.Form):
         self['external_code'].field.initial = training.external_code
 
 
+class ShowFormResponse(forms.Form):
+    id = forms.CharField(label="ID", disabled=True)
+    user = forms.CharField(label='Użytkownik', disabled=True)
+    type = forms.CharField(label='Typ treningu', disabled=True)
+    start_date = forms.CharField(label="Czas rozpoczęcia", disabled=True)
+    time = forms.CharField(label='Długość treningu (w minutach)', disabled=True)
+    provider = forms.CharField(label='Sposób dodania', disabled=True)
+    external_code = forms.CharField(label='Zewnętrzne ID', disabled=True)
+
+    def __init__(self, training):
+        super().__init__()
+        self['id'].field.initial = training.id
+        self['user'].field.initial = training.user
+        self['type'].field.initial = training.type
+        self['start_date'].field.initial = training.get_formatted_start_date()
+        self['time'].field.initial = training.get_formatted_time()
+        self['provider'].field.initial = training.provider
+        self['external_code'].field.initial = training.external_code
+
+
 class AddRunningTrainingForm(forms.Form):
     # Training part
     start_date = forms.DateTimeField(initial=get_current_date_time, label="Czas rozpoczęcia")
@@ -77,51 +99,32 @@ class AddRunningTrainingForm(forms.Form):
 
 
 class GenerateTrainingForm(forms.Form):
-    year_of_birth = forms.IntegerField(label='Proszę podać rok urodzenia', min_value=1900, max_value=2022)
-    height = forms.IntegerField(label='Proszę podać swój wzrost (w centymetrach)', min_value=100, max_value=250)
-    weight = forms.IntegerField(label='Proszę podać swoją wagę (w kilogramach)', min_value=30, max_value=200)
-    running_years = forms.IntegerField(label="Od ilu lat regularnie biegasz?", min_value=0, max_value=50)
-    training_amount = forms.IntegerField(label="Ile treningów biegowych wykonujesz średnio w ciągu tygodnia?",
-                                         min_value=0, max_value=50)
-    km_amount = forms.IntegerField(
-        label="Ile kilometrów pokonujesz średnio podczas treningów biegowych w ciągu tygodnia?", min_value=0,
-        max_value=50)
-    speed_training_amount = forms.IntegerField(label="Ile razy w ciągu miesiąca wykonujesz trening szybkości?",
-                                               min_value=0, max_value=50)
-    minute_per_km_speed_training_id = forms.ChoiceField(
-        label="W jakim zakresie tempa wykonujesz trening szybkości? (w minutach na kilometr)")
-    threshold_training_amount = forms.IntegerField(label="Ile razy w ciągu miesiąca wykonujesz trening progowy?",
-                                                   min_value=0, max_value=50)
-    minute_per_km_threshold_training_id = forms.ChoiceField(
-        label="W jakim zakresie tempa wykonujesz trening progowy? (w minutach na kilometr)")
-    interval_training_amount = forms.IntegerField(label="Ile razy w ciągu miesiąca wykonujesz trening interwałowy?",
-                                                  min_value=0, max_value=50)
-    minute_per_km_interval_training_id = forms.ChoiceField(
-        label="W jakim zakresie tempa wykonujesz trening interwałowy? (w minutach na kilometr)")
-    run_up_training_amount = forms.IntegerField(label="Ile razy w ciągu miesiąca wykonujesz podbiegi?", min_value=0,
-                                                max_value=50)
-    minute_per_km_run_up_training_id = forms.ChoiceField(
-        label="W jakim zakresie tempa wykonujesz podbiegi? (w minutach na kilometr)")
-    runway_amount = forms.IntegerField(label="Ile razy w ciągu miesiąca wykonujesz wybieganie?", min_value=0,
-                                       max_value=50)
-    km_per_runway = forms.IntegerField(
-        label="Jaka jest średnia ilość kilometrów wykonywana podczas jednego wybiegania?", min_value=0, max_value=50)
-    minute_per_km_runway_id = forms.ChoiceField(
-        label="W jakim zakresie tempa wykonujesz wybieganie? (w minutach na kilometr)")
-    other_trainings = forms.BooleanField(label="Czy wykonujesz treningi inne niż biegowe?", required=False)
-    other_trainings_amount = forms.IntegerField(
-        label="Ile razy w ciągu miesiąca wykonujesz powyższe treningi? (sumarycznie)")
-    other_trainings_time = forms.IntegerField(label="Ile minut średnio trwa pojedynczy trening?", min_value=0,
-                                              max_value=50)
-    wellness = forms.BooleanField(label="Czy wykonujesz aktywności związane z odnową biologiczną?", required=False)
-    wellness_amount = forms.IntegerField(
-        label="Ile razy w ciągu miesiąca wykonujesz powyższe aktywności? (sumarycznie)", min_value=0, max_value=50)
-    detraining_amount = forms.IntegerField(label="Ile razy w ciągu roku stosujesz roztrenowanie?", min_value=0,
-                                           max_value=50)
-    detraining_days = forms.IntegerField(label="Jaki jest Twój średni czas trwania roztrenowania? (w dniach)",
-                                         min_value=0, max_value=50)
-    warmup_id = forms.ChoiceField(label="Czy stosujesz rozgrzewkę przed treningiem biegowym?")
-    warmup_time = forms.IntegerField(label="Jaki jest średni czas trwania rozgrzewki? (w minutach)", min_value=0,
+    year_of_birth = forms.IntegerField(label=Q_YEAR_OF_BIRTH, min_value=1900, max_value=2022)
+    height = forms.IntegerField(label=Q_HEIGHT, min_value=100, max_value=250)
+    weight = forms.IntegerField(label=Q_WEIGHT, min_value=30, max_value=200)
+    running_years = forms.IntegerField(label=Q_RUNNING_YEARS, min_value=0, max_value=50)
+    training_amount = forms.IntegerField(label=Q_TRAINING_AMOUNT, min_value=0, max_value=50)
+    km_amount = forms.IntegerField(label=Q_KM_AMOUNT, min_value=0, max_value=50)
+    speed_training_amount = forms.IntegerField(label=Q_SPEED_TRAINING_AMOUNT, min_value=0, max_value=50)
+    minute_per_km_speed_training_id = forms.ChoiceField(label=Q_MINUTE_PER_KM_SPEED_TRAINING)
+    threshold_training_amount = forms.IntegerField(label=Q_THRESHOLD_TRAINING_AMOUNT, min_value=0, max_value=50)
+    minute_per_km_threshold_training_id = forms.ChoiceField(label=Q_MINUTE_PER_KM_THRESHOLD_TRAINING)
+    interval_training_amount = forms.IntegerField(label=Q_INTERVAL_TRAINING_AMOUNT, min_value=0, max_value=50)
+    minute_per_km_interval_training_id = forms.ChoiceField(label=Q_MINUTE_PER_KM_INTERVAL_TRAINING)
+    run_up_training_amount = forms.IntegerField(label=Q_RUN_UP_TRAINING_AMOUNT, min_value=0, max_value=50)
+    minute_per_km_run_up_training_id = forms.ChoiceField(label=Q_MINUTE_PER_KM_RUN_UP_TRAINING)
+    runway_amount = forms.IntegerField(label=Q_RUNWAY_AMOUNT, min_value=0, max_value=50)
+    km_per_runway = forms.IntegerField(label=Q_KM_PER_RUNWAY, min_value=0, max_value=50)
+    minute_per_km_runway_id = forms.ChoiceField(label=Q_MINUTE_PER_KM_RUNWAY)
+    other_trainings = forms.BooleanField(label=Q_OTHER_TRAININGS, required=False)
+    other_trainings_amount = forms.IntegerField(label=Q_OTHER_TRAININGS_AMOUNT)
+    other_trainings_time = forms.IntegerField(label=Q_OTHER_TRAININGS_TIME, min_value=0, max_value=50)
+    wellness = forms.BooleanField(label=Q_WELLNESS, required=False)
+    wellness_amount = forms.IntegerField(label=Q_WELLNESS_AMOUNT, min_value=0, max_value=50)
+    detraining_amount = forms.IntegerField(label=Q_DETRAINING_AMOUNT, min_value=0, max_value=50)
+    detraining_days = forms.IntegerField(label=Q_DETRAINING_DAYS, min_value=0, max_value=50)
+    warmup_id = forms.ChoiceField(label=Q_WARMUP)
+    warmup_time = forms.IntegerField(label=Q_WARMUP_TIME, min_value=0,
                                      max_value=50)
 
     def __init__(self):
